44771e4ce2a31046e766c93d5cdc24b78b270eea
david850519 <david850519@gmail.com>
2017-06-06 00:29:24 +0800

    Add files via upload

---
+++ web_final/js/all.js
@@ -0,0 +1,41 @@
+
+
+$(function(){
+
+	$( ".menu" ).click(function() {
+  
+ 	 $('.shoes').slideToggle();
+  
+	});
+
+});
+
+$(function(){
+
+	$( ".us" ).click(function() {
+  
+ 	 $('.f1').slideToggle();
+  
+	});
+
+});
+
+$(function(){
+
+	$( ".mem" ).click(function() {
+  
+ 	 $('.f2').slideToggle();
+  
+	});
+
+});
+
+$(function(){
+
+	$( ".cen" ).click(function() {
+  
+ 	 $('.f3').slideToggle();
+  
+	});
+
+});
