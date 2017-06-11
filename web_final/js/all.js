1e916c621c89e9a53385ccaecb637b9948da19a5
david850519 <david850519@gmail.com>
2017-06-11 18:01:26 +0800

    Add files via upload

--- web_final/js/all.js
+++ web_final/js/all.js
@@ -1,35 +1,46 @@
-	
-	  	var config = {
-	    apiKey: "AIzaSyC29IcXEjMcz1GMWIb93UPkJBKmRexvowM",
-	    authDomain: "facebooklogin-f6d7c.firebaseapp.com",
-	    databaseURL: "https://facebooklogin-f6d7c.firebaseio.com",
-	    projectId: "facebooklogin-f6d7c",
-	    storageBucket: "facebooklogin-f6d7c.appspot.com",
-	    messagingSenderId: "269057605882"
-	  };
-	  function login(){
-	 		var provider = new firebase.auth.FacebookAuthProvider();
-		  	provider.setCustomParameters({
-			  'display': 'popup'
-			});
-			firebase.auth().signInWithPopup(provider).then(function(result) {
-			  // This gives you a Facebook Access Token. You can use it to access the Facebook API.
-			  var token = result.credential.accessToken;
-			  // The signed-in user info.
-			  var user = result.user;
-			  // ...
-			}).catch(function(error) {
-			  // Handle Errors here
-			  var errorCode = error.code;
-			  var errorMessage = error.message;
-			  // The email of the user's account used.
-			  var email = error.email;
-			  // The firebase.auth.AuthCredential type that was used.
-			  var credential = error.credential;
-			  // ...
-			});
-		  }
+var config = {
+	apiKey: "AIzaSyC29IcXEjMcz1GMWIb93UPkJBKmRexvowM",
+	authDomain: "facebooklogin-f6d7c.firebaseapp.com",
+	databaseURL: "https://facebooklogin-f6d7c.firebaseio.com",
+	projectId: "facebooklogin-f6d7c",
+	storageBucket: "facebooklogin-f6d7c.appspot.com",
+	messagingSenderId: "269057605882"
+	};
 
+function login(){
+	var provider = new firebase.auth.FacebookAuthProvider();
+	provider.setCustomParameters({
+	  'display': 'popup'
+	});
+	firebase.auth().signInWithPopup(provider).then(function(result) {
+	// This gives you a Facebook Access Token. You can use it to access the Facebook API.
+	var token = result.credential.accessToken;
+	// The signed-in user info.
+	var user = result.user;
+	// ...
+	}).catch(function(error) {
+	// Handle Errors here
+	var errorCode = error.code;
+	var errorMessage = error.message;
+	// The email of the user's account used.
+	var email = error.email;
+	// The firebase.auth.AuthCredential type that was used.
+	var credential = error.credential;
+	// ...
+	});
+	}
+
+function GetData(){
+	firebase.auth().onAuthStateChanged(function(user) {
+		  if (user) {
+		    // User is signed in.
+		    alert("Yes");
+		  } else {
+		    // No user is signed in.
+		    alert("No");
+		  }
+		});
+	}	
 
 $(function(){
 
