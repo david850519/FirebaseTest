97f185ff05c277b87a37523fd225cf725e51d24f
david850519 <david850519@gmail.com>
2017-06-13 00:23:54 +0800

    Add files via upload

--- all.js
+++ all.js
@@ -38,6 +38,22 @@ function setCookie(name){
 	document.cookie="username="+name+";"+expires+";path=/";
 }
 
+function getCookie(c_name)
+{
+if (document.cookie.length>0)
+  {
+  c_start=document.cookie.indexOf(c_name + "=")
+  if (c_start!=-1)
+    { 
+    c_start=c_start + c_name.length+1 
+    c_end=document.cookie.indexOf(";",c_start)
+    if (c_end==-1) c_end=document.cookie.length
+    return unescape(document.cookie.substring(c_start,c_end))
+    } 
+  }
+return ""
+}
+
 function checkCookie(){
 	var username=getCookie("username");
 	if(username!=""){
