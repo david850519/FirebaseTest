64c832e513dcf0f76bd6f4f28381615f1412dd5f
david850519 <david850519@gmail.com>
2017-06-12 23:19:19 +0800

    Add files via upload

---
+++ all.js
@@ -0,0 +1,31 @@
+var config = {
+	apiKey: "AIzaSyC29IcXEjMcz1GMWIb93UPkJBKmRexvowM",
+	authDomain: "facebooklogin-f6d7c.firebaseapp.com",
+	databaseURL: "https://facebooklogin-f6d7c.firebaseio.com",
+	projectId: "facebooklogin-f6d7c",
+	storageBucket: "facebooklogin-f6d7c.appspot.com",
+	messagingSenderId: "269057605882"
+	};
+
+function login(){
+	var provider = new firebase.auth.FacebookAuthProvider();
+	provider.setCustomParameters({
+	  'display': 'popup'
+	});
+	firebase.auth().signInWithPopup(provider).then(function(result) {
+		var token = result.credential.accessToken;
+		var user = result.user;
+		var name,email,photoUrl,uid;
+		name = user.displayName;
+  		email = user.email;
+  		photoUrl = user.photoURL;
+  		uid = user.uid;
+  		console.log(uid);
+  		document.getElementById('header').innerHTML=name;
+	}).catch(function(error) {
+		var errorCode = error.code;
+		var errorMessage = error.message;
+		var email = error.email;
+		var credential = error.credential;
+	});
+	}
