6f7bc72aca48b12aaea3b0a97ed4d82f1dc5f9e0
david850519 <david850519@gmail.com>
2017-06-13 10:59:18 +0800

    Add files via upload

--- all.js
+++ all.js
@@ -21,7 +21,8 @@ function login(){
   		photoUrl = user.photoURL;
   		uid = user.uid;
   		console.log(uid);
-  		document.getElementById('header').innerHTML=name;
+  		setCookie(name);
+
 	}).catch(function(error) {
 		var errorCode = error.code;
 		var errorMessage = error.message;
@@ -29,3 +30,42 @@ function login(){
 		var credential = error.credential;
 	});
 	}
+
+function setCookie(name){
+	var d = new Date();
+	d.setTime=(d.getTime()+1000);
+	var expires="expires"+d.toUTCString();
+	document.cookie="username="+name+";"+expires+";path=/";
+}
+
+function getCookie(c_name)
+{
+if (document.cookie.length>0)
+  {
+  c_start=document.cookie.indexOf(c_name + "=")
+  if (c_start!=-1)
+    { 
+    c_start=c_start + c_name.length+1 
+    c_end=document.cookie.indexOf(";",c_start)
+    if (c_end==-1) c_end=document.cookie.length
+    return unescape(document.cookie.substring(c_start,c_end))
+    } 
+  }
+return ""
+}
+
+function checkCookie(){
+	var username=getCookie("username");
+	if(username!=""){
+		console.log("user:"+username);
+		return 0;
+	}else{
+		console.log("no user");
+		return 1;
+	}
+}
+
+function changeheader(name,img){
+	console.log("name:"+name);
+	console.log("img:"+img);
+}
